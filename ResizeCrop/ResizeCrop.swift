cd4e74073f92429a0ea4fb9dfab8036f17d30a6a
Maxim Naumov <ultimate.deej@gmail.com>
2017-01-22 00:16:28 +0300

    doc comments

--- ResizeCrop/ResizeCrop.swift
+++ ResizeCrop/ResizeCrop.swift
@@ -1,5 +1,40 @@
 import UIKit
 
+/**
+ Functions for resizing an image to fill a specific `CGSize` or `UIView`'s frame.
+ - Important: Aspect ratio is always maintained.
+
+ **Examples**
+ ````
+ ResizeCrop.resize(
+     image: myImage,
+     toFillView: myView,
+     gravity: .bottom)
+ ````
+ ````
+ ResizeCrop.resize(
+     image: cgImage,
+     toPixelSize: CGSize(width: 100, height: 100),
+     gravity: .center,
+     quality: .default)
+ ````
+
+ Generally, you provide two things besides the image:
+ - *Gravity*: determines which part of the image will be cropped
+ - *Target*: an area the image should fill
+
+ There are a couple of other resizing options:
+ - *Scale* (e.g. 1x, 2x). Implied or specified explicitly
+ - *Interpolation quality*. `.high` by default
+
+ ---------------------------------------------------------------
+
+ - Note: As the name of the library implies, images are cropped.
+ Specifically, the library does almost the same thing as `UIImageView`
+ with content mode set to **Aspect Fill** except that *you can choose
+ which part of the image to crop*, and the image is actually resampled
+ therefore consuming less memory.
+ */
 public struct ResizeCrop {
     public enum Gravity {
         case center
@@ -13,6 +48,15 @@ public struct ResizeCrop {
 
     // MARK: - Low Level Functions
 
+    /**
+     Resizes an image to the specified size in pixels
+
+     - Parameters:
+       - image: Source image
+       - pixelSize: Desired size of the resulting image in pixels
+       - gravity: Which part of the `image` to crop
+       - quality: Interpolation quality for a graphics context
+     */
     public static func resize(image: CGImage, toPixelSize pixelSize: CGSize, gravity: Gravity, quality: CGInterpolationQuality = .high) -> CGImage {
         let sourceRatio = ratio(width: CGFloat(image.width), height: CGFloat(image.height))
         let drawLocation = calculateDrawLocation(sourceRatio: sourceRatio, rectToFill: pixelSize, gravity: gravity)
@@ -24,6 +68,15 @@ public struct ResizeCrop {
         return context.makeImage()!
     }
 
+    /**
+     Resizes an image to the specified size in pixels
+
+     - Parameters:
+       - image: Source image
+       - pixelSize: Desired size of the resulting image in pixels
+       - gravity: Which part of the `image` to crop
+       - quality: Interpolation quality for a graphics context
+     */
     public static func resize(image: UIImage, toPixelSize pixelSize: CGSize, gravity: Gravity, quality: CGInterpolationQuality = .high) -> UIImage {
         let resized = resize(image: image.cgImage!, toPixelSize: pixelSize, gravity: gravity, quality: quality)
         return UIImage(cgImage: resized)
@@ -31,16 +84,48 @@ public struct ResizeCrop {
 
     // MARK: - High Level Functions
 
+    /**
+     Resizes an image given a target size in points and a scaleFactor
+
+     - Parameters:
+       - image: Source image
+       - size: Desired size of the resulting image in points
+       - scaleFactor: Scale factor of the resulting image. Used in conjunction with `size` to determine the size of the result
+       - gravity: Which part of the `image` to crop
+     */
     public static func resize(image: UIImage, toSize size: CGSize, scaleFactor: CGFloat, gravity: Gravity) -> UIImage {
         let pixelSize = CGSize(width: size.width * scaleFactor, height: size.height * scaleFactor)
         let resized = resize(image: image.cgImage!, toPixelSize: pixelSize, gravity: gravity)
         return UIImage(cgImage: resized, scale: scaleFactor, orientation: image.imageOrientation)
     }
 
+    /**
+     Resizes an image to the specified size
+
+     - Parameters:
+       - image: Source image
+       - size: Desired size of the resulting image. Units depend on the `image` scale
+       - gravity: Which part of the `image` to crop
+     */
     public static func resize(image: UIImage, toSize size: CGSize, gravity: Gravity) -> UIImage {
         return resize(image: image, toSize: size, scaleFactor: image.scale, gravity: gravity)
     }
 
+
+    /**
+     Resizes an image to the size of a view's frame
+
+     - Parameters:
+       - image: Source image
+       - view: The view whose size is used for the result
+       - gravity: Which part of the `image` to crop
+
+     - Requires:
+     `view.window != nil`
+
+     - Note:
+       Pixel size of the result is determined by the scale factor the `view`'s screen
+     */
     public static func resize(image: UIImage, toFillView view: UIView, gravity: Gravity) -> UIImage {
         return resize(image: image, toSize: view.frame.size, scaleFactor: view.window!.screen.scale, gravity: gravity)
     }
